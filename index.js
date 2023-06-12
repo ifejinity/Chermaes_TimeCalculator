feb8047fdb5b8bdcd074be08a590d4df38d23e40
ifejinity <mrlonzanida08@gmail.com>
2023-06-12 08:03:33 +0800

    commit 3

--- index.js
+++ index.js
@@ -33,50 +33,67 @@ for(let i = 0; i <= 60; i++){
 const compute = document.querySelector("#compute");
 const from = document.querySelector("#from");
 const textTime = document.querySelector("#texttime");
+const modalUnable = document.querySelector("#modalUnable");
+const modalBtn = document.querySelector("#btnUnable");
+
 compute.addEventListener('click', function(){
-    // Create a new Date object
-    var initialTime = new Date();
+    if(fromHours.selectedIndex != 0 &&
+        fromMin.selectedIndex != 0 &&
+        addHours.selectedIndex != 0 &&
+        addMins.selectedIndex != 0 &&
+        from.selectedIndex != 0){
+        // Create a new Date object
+        var initialTime = new Date();
 
-    // Set the initial time
-    if(from.value == "am"){
-        if(fromHours.value == 12){
-            initialTime.setHours(0);
+        // Set the initial time
+        if(from.value == "am"){
+            if(fromHours.value == 12){
+                initialTime.setHours(0);
+            }
+            else{
+                initialTime.setHours(fromHours.value);
+            }
         }
-        else{
-            initialTime.setHours(fromHours.value);
+        if(from.value == "pm"){
+            if(fromHours.value == 12){
+                initialTime.setHours(fromHours.value);
+            }
+            else{
+                initialTime.setHours(12 + parseInt(fromHours.value));
+            }
         }
+
+        initialTime.setMinutes(fromMin.value);
+
+        // add hours
+        initialTime.setHours(initialTime.getHours() + parseInt(addHours.value));
+        // Add minutes
+        initialTime.setMinutes(initialTime.getMinutes() + parseInt(addMins.value));
+
+        // Format the time as a string
+        var updatedTimeString = initialTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit',  hour12: true});
+
+        // Display the updated time
+        // console.log(updatedTimeString);
+
+        // display the updated time on textarea
+        textTime.value = updatedTimeString;
     }
-    if(from.value == "pm"){
-        if(fromHours.value == 12){
-            initialTime.setHours(fromHours.value);
-        }
-        else{
-            initialTime.setHours(12 + parseInt(fromHours.value));
-        }
+    else{
+        modalUnable.style.display = "flex";
     }
+});
 
-    initialTime.setMinutes(fromMin.value);
-
-    // add hours
-    initialTime.setHours(initialTime.getHours() + parseInt(addHours.value));
-    // Add minutes
-    initialTime.setMinutes(initialTime.getMinutes() + parseInt(addMins.value));
-
-    // Format the time as a string
-    var updatedTimeString = initialTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit',  hour12: true});
-
-    // Display the updated time
-    // console.log(updatedTimeString);
-
-    // display the updated time on textarea
-    textTime.value = updatedTimeString;
+modalBtn.addEventListener('click', function(){
+    modalUnable.style.display = "none";
 });
 
 const reset = document.querySelector("#reset")
 reset.addEventListener('click', function(){
-    fromHours.value = 1;
-    fromMin.value = 0;
-    addHours.value = 0;
-    addMins.value = 0;
+    fromHours.selectedIndex = 0;
+    fromMin.selectedIndex = 0;
+    addHours.selectedIndex = 0;
+    addMins.selectedIndex = 0;
+    from.selectedIndex = 0;
     textTime.value = "";
 })
